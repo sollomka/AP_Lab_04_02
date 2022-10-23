914c5b469aaff7a2e4d9756ca36bdc1a1b13089d
sollomka <solomiia.holovata.ri.2022@lpnu.ua>
2022-10-23 15:28:29 +0300

    first

---
+++ АП_РІ-11_Головата_ЛР_04_2/AP_Lab_04_2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = (1. / abs(x + 2)) + 1;
+		if (x < 1)
+			B = 7 * pow((x), 2) + x - 8;
+		else
+			if (1 <= x && x <= 4) 
+				B = (1. / tan((x + 4) / sqrt(11))) + 3;
+			else
+				B = sqrt(1 + (abs(pow(cos(x), 3))));
+		y = A - B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
